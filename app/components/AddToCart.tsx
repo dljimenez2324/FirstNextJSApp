71d63bccfd911965d6da919b634b439964502b37
dljimenez2324 <dljimenez2324@gmail.com>
2024-11-19 19:45:17 -0800

    added components and styling

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,7 @@
+'use client'
+const AddToCart = () => {
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("Clicked")}>Add to Cart</button>
+  )
+}
+export default AddToCart
